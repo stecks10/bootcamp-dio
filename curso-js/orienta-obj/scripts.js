e067f70c93a76da25057fe593c1881d635910a46
Vitor Nunes <stecks10@gmail.com>
2021-12-13 13:36:58 -0300

    contabancaria

---
+++ curso-js/orienta-obj/scripts.js
@@ -0,0 +1,70 @@
+class ContaBancaria {
+  constructor(agencia, numero, tipo) {
+    this.agencia = agencia;
+    this.numero = numero;
+    this.tipo = tipo;
+    this._saldo = 0;
+  }
+
+  sacar(valor) {
+    if (valor > this._saldo) {
+      return console.log('Saque negado; saldo insuficiente!');
+    }
+
+    this._saldo = this._saldo - valor;
+    return this._saldo;
+  }
+
+  depositar(valor) {
+    this._saldo = this._saldo + valor;
+    return this._saldo;
+  }
+
+  set saldo(valor) {
+    this._saldo = valor;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+}
+
+class ContaCorrente extends ContaBancaria {
+  constructor(agencia, numero, cartaoCredito) {
+    super(agencia, numero);
+    this.tipo = 'corrente';
+    this._cartaoCredito = cartaoCredito;
+  }
+  set cartaoCredito(valor) {
+    this._cartaoCredito = valor;
+  }
+
+  get cartaoCredito() {
+    return this._cartaoCredito;
+  }
+}
+
+class ContaPoupanca extends ContaBancaria {
+  constructor(agencia, numero) {
+    super(agencia, numero);
+    this.tipo = 'poupança';
+  }
+}
+
+class ContaUniversitaria extends ContaBancaria {
+  constructor(agencia, numero) {
+    super(agencia, numero);
+    this.tipo = 'universitaria';
+  }
+
+  sacar(valor) {
+    if (valor > 500) {
+      return 'operacao negada!';
+    }
+
+    this._saldo = this.saldo - valor;
+    return this.saldo;
+  }
+}
+/* const minhaConta = new ContaCorrente(1, 211, true);
+const contaUni = new ContaUniversitaria(2, 333); */
